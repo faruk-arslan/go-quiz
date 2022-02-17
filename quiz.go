36da592218f5a18c29c6ca3b0e3efcb6fb291ed8
Faruk Arslan <farukarslan011@gmail.com>
2022-02-18 01:25:56 +0300

    Add timeout for questions

--- quiz.go
+++ quiz.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"reflect"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -24,53 +24,54 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(records)
-	fmt.Println(len(records))
-	fmt.Println(reflect.TypeOf(records))
+	// fmt.Println(records)
+	// fmt.Println(len(records))
+	// fmt.Println(reflect.TypeOf(records))
 
-	rt := reflect.TypeOf(records)
-	switch rt.Kind() {
-	case reflect.Slice:
-		fmt.Println(records, "is a slice with element type", rt.Elem())
-	case reflect.Array:
-		fmt.Println(records, "is an array with element type", rt.Elem())
-	default:
-		fmt.Println(records, "is something else entirely")
-	}
+	// rt := reflect.TypeOf(records)
+	// switch rt.Kind() {
+	// case reflect.Slice:
+	// 	fmt.Println(records, "is a slice with element type", rt.Elem())
+	// case reflect.Array:
+	// 	fmt.Println(records, "is an array with element type", rt.Elem())
+	// default:
+	// 	fmt.Println(records, "is something else entirely")
+	// }
 
 	var point float32 = 0
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+	done := make(chan bool)
+
 	for i := 0; i < len(records); i++ {
 		var tempAnswer int
-		fmt.Printf("Question %d: %v=? \n", i+1, records[i][0])
-		fmt.Scan(&tempAnswer)
-		// convert the answer string (from slice) to int
-		intAnswer, err := strconv.Atoi(records[i][1])
-		if err != nil {
-			log.Fatal("Can not convert the answer to int")
-		}
-		if tempAnswer == intAnswer {
-			point += 8.333333333333333
+
+		go func() {
+			fmt.Printf("Question %d: %v=? \n", i+1, records[i][0])
+			fmt.Scan(&tempAnswer)
+
+			// convert the answer string (from slice) to int
+			intAnswer, err := strconv.Atoi(records[i][1])
+			if err != nil {
+				log.Fatal("Can not convert the answer to int")
+			}
+			if tempAnswer == intAnswer {
+				point += 8.333333333333333
+			}
+			// continue to next iteration
+			done <- true
+		}()
+
+		select {
+		case <-done:
+			// if done true, continue to the next iteration
+			fmt.Println("Next question!")
+			continue
+		case t := <-ticker.C:
+			// ticker.C is triggered after timeout
+			fmt.Println("Current time: ", t)
+			fmt.Println("Time is out, next question: ")
 		}
 	}
 	fmt.Println("Your score is: ", point)
-
-	// for i := 0; i < len(records); i++ {
-	// 	fmt.Println(answers[i])
-	// }
-
-	// var jsonBlob = []byte(`[
-	// 	{"Question": "5*2", "Answer": "10"}
-	// ]`)
-
-	// type Question struct {
-	// 	Question string
-	// 	Answer   string
-	// }
-
-	// var questions []Question
-	// err := json.Unmarshal(data, &questions)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Printf("%+v", questions)
 }
